800ee418f7e16d6db8b3326448260e3ea024c42c
Rishabh Mukund <rishabh.m96@gmail.com>
2022-02-09 18:50:52 -0500

    Implemented least squares to track the ball using only low level cv2 functions

--- least_square_without_noise.py
+++ least_square_without_noise.py
@@ -1,37 +1,41 @@
+# Object-Tracking
+# Copyright (c) 2022 Rishabh Mukund
+# MIT License
+#
+# Description: Finding the top and bottom of the red ball in each frame and
+#              performing least squares for estimating the path of the ball
+
 import matplotlib.pyplot as plt
 import numpy as np
 import cv2
 
 cap = cv2.VideoCapture('Videos/ball_video1.mp4')
 
-x_top = []
-y_top = []
-x_bottom = []
-y_bottom = []
+x = []
+y = []
 
 while (cap.isOpened()):
     ret, frame = cap.read()
     if ret:
         red = frame[:, :, 2]
-        ret, thresh = cv2.threshold(red, 220, 255, cv2.THRESH_BINARY_INV)
-        x, y, w, h = cv2.boundingRect(thresh)
+        ret, thresh = cv2.threshold(red, 230, 255, cv2.THRESH_BINARY_INV)
+        x_1, y_1, w, h = cv2.boundingRect(thresh)
         row, col = np.where(thresh == 255)
 
         top = [col[0], row[0]]
         bottom = [col[-1], row[-1]]
 
-        x_top.append(top[0])
-        y_top.append(top[1])
-        x_bottom.append(bottom[0])
-        y_bottom.append(bottom[1])
+        x.append(top[0])
+        y.append(top[1])
+        x.append(bottom[0])
+        y.append(bottom[1])
 
         cv2.circle(frame, top, 8, (0, 255, 0), -1)
         cv2.circle(frame, bottom, 8, (0, 255, 0), -1)
 
         cv2.imshow('frame', frame)
-        cv2.imshow('after thresholding', thresh)
 
-        if cv2.waitKey(0) & 0xFF == ord('q'):
+        if cv2.waitKey(1) & 0xFF == ord('q'):
             break
     else:
         break
@@ -39,7 +43,13 @@ while (cap.isOpened()):
 cap.release()
 cv2.destroyAllWindows()
 
-plt.plot(x_top, y_top, 'ro')
-plt.plot(x_bottom, y_bottom, 'bo')
+plt.plot(x, y, 'ro')
 
+
+A = np.transpose(np.vstack((np.power(x, 2), x, np.ones(len(x)))))
+X = np.matmul(np.matmul(np.linalg.inv(np.matmul(np.transpose(A), A)),
+                        np.transpose(A)), y)
+
+plt.plot(x, np.matmul(A, X), 'g-')
+plt.gca().invert_yaxis()
 plt.show()
